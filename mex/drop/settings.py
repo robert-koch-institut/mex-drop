012adae6a74bac0e4fb27e612215a9124df6f0cb
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2023-06-30 12:52:06 +0200

    add settings

---
+++ mex/drop/settings.py
@@ -0,0 +1,27 @@
+from pydantic import Field
+
+from mex.common.settings import BaseSettings
+
+
+class DropSettings(BaseSettings):
+    """Settings definition for the drop server."""
+
+    drop_host: str = Field(
+        "localhost",
+        min_length=1,
+        max_length=250,
+        description="Host that the drop server will run on.",
+        env="MEX_DROP_HOST",
+    )
+    drop_port: int = Field(
+        8080,
+        gt=0,
+        lt=65536,
+        description="Port that the drop server should listen on.",
+        env="MEX_DROP_PORT",
+    )
+    drop_root_path: str = Field(
+        "data",
+        description="Root path that the drop server should run under.",
+        env="MEX_DROP_ROOT_PATH",
+    )
