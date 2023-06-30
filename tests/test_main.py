b348527ec6969e41f22942c6ef2ad7ff2daffb0d
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2023-06-30 14:25:35 +0200

    parametrize test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 
 
@@ -7,9 +8,21 @@ def test_read_root(client: TestClient) -> None:
     assert response.json() == {"Hello": "World"}
 
 
-def test_post_data(client: TestClient) -> None:
-    x_system = "foo"
-    entity = "bar"
-    expected_response_code = 200
+@pytest.mark.parametrize(
+    "x_system, entity, expected_response_code",
+    [
+        ("foo", "bar", 200),
+        ("invalid x_system", "bar", 422),
+        ("foo", "invalid entity", 422),
+    ],
+    ids=(
+        "valid",
+        "invalid x_system",
+        "invalid entity",
+    ),
+)
+def test_post_data(
+    client: TestClient, x_system: str, entity: str, expected_response_code: int
+) -> None:
     response = client.post(f"/{x_system}/{entity}", json={"asd": "def"})
     assert response.status_code == expected_response_code, response.text
