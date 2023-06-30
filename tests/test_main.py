78d3b90eec403c32b047eed2e40a13ec61ac6a31
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2023-06-30 14:13:51 +0200

    add test for posting data

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,3 +5,11 @@ def test_read_root(client: TestClient) -> None:
     response = client.get("/")
     assert response.status_code == 200, response.text
     assert response.json() == {"Hello": "World"}
+
+
+def test_post_data(client: TestClient) -> None:
+    x_system = "foo"
+    entity = "bar"
+    expected_response_code = 200
+    response = client.post(f"/{x_system}/{entity}", json={"asd": "def"})
+    assert response.status_code == expected_response_code, response.text
