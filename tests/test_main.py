f4a71e1262956a7b8b200ca49a926ed90a615334
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2023-07-05 14:19:48 +0200

    fix tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ from fastapi.testclient import TestClient
 
 
 def test_read_root(client: TestClient) -> None:
-    response = client.get("/")
+    response = client.get("/api/v0/")
     assert response.status_code == 200, response.text
     assert response.json() == {"Hello": "World"}
 
@@ -11,7 +11,7 @@ def test_read_root(client: TestClient) -> None:
 @pytest.mark.parametrize(
     "x_system, entity, expected_response_code",
     [
-        ("foo", "bar", 200),
+        ("foo", "bar", 201),
         ("invalid x_system", "bar", 422),
         ("foo", "invalid entity", 422),
     ],
@@ -24,5 +24,5 @@ def test_read_root(client: TestClient) -> None:
 def test_post_data(
     client: TestClient, x_system: str, entity: str, expected_response_code: int
 ) -> None:
-    response = client.post(f"/{x_system}/{entity}", json={"asd": "def"})
+    response = client.post(f"/api/v0/{x_system}/{entity}", json={"asd": "def"})
     assert response.status_code == expected_response_code, response.text
