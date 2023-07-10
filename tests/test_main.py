18491af90d244f7a7aaf3346d38d7020473776f3
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2023-07-10 12:59:35 +0200

    mock sink in API tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,17 @@
-import json
 from pathlib import Path
+from unittest.mock import MagicMock, call
 
 import pytest
 from fastapi.testclient import TestClient
+from pytest import MonkeyPatch
 
+import mex
 from mex.drop.settings import DropSettings
+
+
+class AsyncMock(MagicMock):
+    async def __call__(self, *args, **kwargs):
+        return super(AsyncMock, self).__call__(*args, **kwargs)
 
 
 def test_read_root(client: TestClient) -> None:
@@ -27,17 +34,15 @@ def test_read_root(client: TestClient) -> None:
     ),
 )
 def test_post_data(
-    client: TestClient, x_system: str, entity: str, expected_response_code: int
+    client: TestClient,
+    x_system: str,
+    entity: str,
+    expected_response_code: int,
+    monkeypatch: MonkeyPatch,
+    settings: DropSettings,
 ) -> None:
-    response = client.post(f"/api/v0/{x_system}/{entity}", json={"asd": "def"})
-    assert response.status_code == expected_response_code, response.text
-
-
-def test_post_data_is_written_to_file(
-    client: TestClient, settings: DropSettings
-) -> None:
-    relative_path = "wasd/def"
-    expected_file = Path(settings.drop_root_path, relative_path + ".json")
+    mocked_sink = AsyncMock()
+    monkeypatch.setattr(mex.drop.main, "json_sink", mocked_sink)
     expected_content = {
         "asd": "def",
         "foo": 1,
@@ -45,9 +50,8 @@ def test_post_data_is_written_to_file(
         "list": [1, 2, 3],
         "dict": {"a": "b"},
     }
-    response = client.post("/api/v0/" + relative_path, json=expected_content)
-    assert response.status_code == 201
-    assert expected_file.is_file()
-    with expected_file.open(encoding="utf-8") as handle:
-        content = json.load(handle)
-    assert content == expected_content
+    expected_file = Path(settings.drop_root_path, x_system, entity + ".json")
+    response = client.post(f"/api/v0/{x_system}/{entity}", json=expected_content)
+    assert response.status_code == expected_response_code, response.text
+    if 200 <= response.status_code < 300:
+        assert mocked_sink.call_args == call(expected_content, expected_file)
