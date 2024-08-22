140187c0d1408909586a8c5930249a594e6fb7e9
Zehra Ciftci <CiftciZ@rki.de>
2024-08-22 10:37:27 +0200

    initial push
    Signed-off-by: Zehra Ciftci <CiftciZ@rki.de>

--- mex/drop/main.py
+++ mex/drop/main.py
@@ -17,21 +17,89 @@ def mex_drop_logo() -> rx.Component:
     )
 
 
+def uploaded_file_display() -> rx.Component:
+    """Displays uploaded files from drop interface."""
+    return rx.table.root(
+        rx.table.header(
+            rx.table.row(
+                rx.table.column_header_cell("Uploaded File", width="80%"),
+                rx.table.column_header_cell("Actions", width="20%"),
+            ),
+        ),
+        rx.table.body(
+            rx.table.row(
+                rx.table.row_header_cell("Test_File.xml", width="80%"),
+                rx.table.cell(
+                    rx.button(
+                        rx.icon(tag="trash-2"),
+                        color_scheme="red",
+                    ),
+                    width="20%",
+                ),
+            )
+        ),
+        width="90%",
+    )
+
+
 def index() -> rx.Component:
     """Return the index for the drop app."""
     return rx.center(
-        rx.card(
-            rx.vstack(
-                mex_drop_logo(),
-                rx.divider(size="4"),
-                rx.text(
-                    "This page is not yet implemented.",
+        rx.container(
+            rx.hstack(
+                rx.card(
+                    rx.vstack(
+                        mex_drop_logo(),
+                        rx.divider(size="4"),
+                        rx.text(
+                            "Upload your files here.",
+                        ),
+                        rx.upload(
+                            rx.vstack(
+                                rx.icon("file-down"),
+                                rx.text(
+                                    "Drag and drop files here or click to select files"
+                                ),
+                                rx.button(
+                                    "Select File",
+                                    color="rgb(107,99,246)",
+                                    bg="white",
+                                    border="1px solid rgb(107,99,246)",
+                                ),
+                                align="center",
+                            ),
+                            border="1px dotted rgb(107,99,246)",
+                            padding="10em",
+                        ),
+                        spacing="4",
+                    ),
+                    top="20vh",
+                    width="50%",
+                    variant="classic",
+                    custom_attrs={"data-testid": "index-card"},
                 ),
-                spacing="4",
+                rx.card(
+                    rx.vstack(
+                        uploaded_file_display(),
+                        rx.hstack(
+                            rx.input(
+                                placeholder="API token",
+                                max_length=50,
+                                id="x-system-input",
+                            ),
+                            rx.input(
+                                placeholder="x-system",
+                                max_length=50,
+                                id="x-system-input",
+                            ),
+                            rx.button("Submit", width="20%", id="submit-button"),
+                        ),
+                    ),
+                    width="50%",
+                    top="20vh",
+                ),
+                top="20vh",
+                width="90%",
             ),
-            top="20vh",
-            width="400px",
-            variant="classic",
-            custom_attrs={"data-testid": "index-card"},
-        ),
+        )
     )
