3d7f55e3b7c3d435636d3dfefee1733363e7c409
Mikail Yaldiz <yaldizm@rki.de>
2025-10-23 08:59:26 +0200

    added test for prometheus metrics endpoint (not passing)

--- tests/api/test_main.py
+++ tests/api/test_main.py
@@ -361,6 +361,19 @@ def test_list_entity_types_forbidden(client: TestClient) -> None:
     assert response.status_code == 403, response.text
 
 
+def test_prometheus_metrics(client: TestClient) -> None:
+    response = client.get("/_system/metrics")
+    assert response.status_code == 200, response.text
+    assert response.text == (
+        """\
+# TYPE backend_api_identity_provider_cache_hits counter
+backend_api_identity_provider_cache_hits 17
+
+# TYPE backend_api_identity_provider_cache_misses counter
+backend_api_identity_provider_cache_misses 7"""
+    )
+
+
 def test_health_check(client: TestClient) -> None:
     response = client.get("/_system/check")
     assert response.status_code == 200, response.text
